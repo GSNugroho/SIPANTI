89b630f149d84f57d71a6384f3188955af3cc179
GSNugroho <gnugroho11@gmail.com>
2019-09-04 09:28:38 +0700

    New

--- application/views/mainmenu.php
+++ application/views/mainmenu.php
@@ -65,7 +65,7 @@
 
 		<hr class="sidebar-divider">
 		<div class="sidebar-heading">
-		Maintenance
+		Pemeliharaan
 		</div>
 			<!-- Perawatan -->
 			<li class="nav-item">
@@ -287,14 +287,14 @@
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
-            <!-- <li class="nav-item dropdown no-arrow">
+            <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-              </a> -->
+                <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span> -->
+                <img class="img-profile rounded-circle" src="<?php echo base_url('assets/bootstrap/image/icno.png')?>">
+              </a> 
               <!-- Dropdown - User Information -->
-              <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <!-- <a class="dropdown-item" href="#">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Profile
                 </a>
@@ -305,19 +305,35 @@
                 <a class="dropdown-item" href="#">
                   <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                   Activity Log
-                </a>
+                </a> -->
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
+                  Keluar
                 </a>
               </div>
-            </li> -->
+            </li>
 
           </ul>
 
         </nav>
         <!-- End of Topbar -->
-
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Yakin keluar ?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Pilih keluar jika sudah yakin</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+          <a class="btn btn-primary" href="<?php echo site_url();?>">Keluar</a>
+        </div>
+      </div>
+    </div>
+  </div>
         <!-- Begin Page Content -->
         <div class="container-fluid">
